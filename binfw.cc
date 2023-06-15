cf1f0e61f89f2d374b0163fb25fc2cbc013d7dba
Jimmer1 <jr195698@gmail.com>
2023-06-15 07:30:50 +0100

    Code tidy

--- binfw.cc
+++ binfw.cc
@@ -2,10 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <span>
 
-
-// LCOV_EXCL_START
 int main([[maybe_unused]] int argc, char** argv) {
     std::ifstream reader(argv[1]);
     std::vector<std::uint8_t> code;
@@ -13,15 +10,10 @@ int main([[maybe_unused]] int argc, char** argv) {
     while (reader >> std::hex >> c) {
         code.push_back(std::uint8_t(c));
     }
-
     if (code.empty()) {
         return -1;
     }
-
-    std::cout << code.size() << std::endl;
-
     std::fstream writer(argv[2], std::ios::out | std::ios::binary);
     writer.write(reinterpret_cast<char*>(&code[0]), code.size());
-
+    return 0;
 }
-// LCOV_EXCL_STOP
